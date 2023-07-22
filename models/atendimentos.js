6df62fa3249c93d6c648048181d7a0a9fa279295
niltonguilhem <nil.guilhem@gmail.com>
2023-07-22 11:40:47 -0300

    first commit

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,17 +2,20 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+
         const atendimentoDatado = {...atendimento, dataCriacao, data}
+
         const sql = 'INSERT INTO Atendimentos SET ?'
 
         conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro) {
-                console.log(erro)
+                res.status(400).json(erro)
             } else {
-                console.log(resultados)
+                res.status(201).json(resultados)
             }
         })
     }
